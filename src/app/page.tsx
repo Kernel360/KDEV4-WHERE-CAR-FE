11d53b1071973d46b3bd297fdad8c2195784ea8b
Laterre <laterre39@naver.com>
2025-03-21 16:29:04 +0900

    feat: cicd용 프리뷰 페이지

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,26 @@
+import Image from "next/image";
+
 export default function Home() {
-  return <></>;
+  return (
+      <>
+        <div className="flex flex-col items-center justify-center min-h-screen gap-4 bg-gray-100">
+          <div className="w-[500px] h-[500px]">
+            <Image
+                className="rounded-lg"
+                src="/images/wherecar_logo2.png"
+                width={500}
+                height={500}
+                alt="어디카!"
+            />
+          </div>
+          <div className="w-[500px] h-[100px] flex justify-center items-center gap-2 flex-col bg-gray-200 rounded-lg">
+            <p className="text-4xl font-bold">🚘 어디카?</p>
+            <p className="text-lg text-slate-400">환영합니다, 기본 배포용 사이트입니다.</p>
+          </div>
+          <div>
+            <p></p>
+          </div>
+        </div>
+      </>
+  );
 }
